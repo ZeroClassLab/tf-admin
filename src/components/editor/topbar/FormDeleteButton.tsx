2b584129cab4e88f5bd6a0373fe702807f41ffe1
zerosheepmoo <zerosheepmoo@gmail.com>
2022-06-21 16:57:10 +0900

    삭제하기 안내

--- src/components/editor/topbar/FormDeleteButton.tsx
+++ src/components/editor/topbar/FormDeleteButton.tsx
@@ -1,20 +1,29 @@
 import React from "react";
 import Button from "@mui/material/Button";
-import { useRecoilValue } from "recoil";
+import { useRecoilValue, useSetRecoilState } from "recoil";
 import {
     editorSubmitModeState,
     postIDwhenEditModeState,
     SubmitMode,
 } from "../recoils";
 import axios from "axios";
+import { currentPageState } from "../../recoils";
 
 const FormDeleteButton = () => {
     const submitMode = useRecoilValue(editorSubmitModeState);
     const postIDwhenEditMode = useRecoilValue(postIDwhenEditModeState);
+    const setCurPage = useSetRecoilState(currentPageState);
     const handleDelete = async () => {
-        await axios.delete(
-            `${process.env.REACT_APP_MAIN_BACK}/story/${postIDwhenEditMode}`
-        );
+        try {
+            await axios.delete(
+                `${process.env.REACT_APP_MAIN_BACK}/story/${postIDwhenEditMode}`
+            );
+            alert("삭제가 완료되었습니다!");
+            setCurPage(0);
+        } catch (e) {
+            console.error(e);
+            alert("삭제에 실패하였습니다!");
+        }
     };
     return (
         <Button
